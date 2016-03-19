558a07f41b304d2996265a8895bf5f0bd20e51d8
praveen <thejavamonk@gmail.com>
2016-03-19 19:25:11 +0530

    tutorial on events and event listeners

--- 07eventsAndEventListeners/timer.js
+++ 07eventsAndEventListeners/timer.js
@@ -2,4 +2,18 @@ function simpleMessage(){
 	alert("this is a message");
 }
 
-setTimeout(simpleMessage, 5000);
+// setTimeout(simpleMessage, 5000);
+
+var myImage = document.getElementById("codeprep");
+var imageArray = ["gangnamcat.PNG", "labtocat.PNG", "codeprep.PNG"];
+var imageIndex = 0;
+
+function changeImage(){
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	imageIndex++;
+	if(imageIndex >= imageArray.length){
+		imageIndex = 0;
+	}
+}
+
+setInterval(changeImage, 5000);
