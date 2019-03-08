3a229c744ace1c79eddea5eb95e63aa2051c22bb
praveen <thejavamonk@gmail.com>
2019-03-08 11:59:50 +0530

    added example of arrays

--- 04typesAndObjects/Arrays.js
+++ 04typesAndObjects/Arrays.js
@@ -18,3 +18,27 @@ var multipleValues = Array(5);
 // Arrays is javascript can be created with a fixed size but normally when no size
 // attribute is set they dynamically resize.
 
+// methods of an array
+
+var jhon = ['Jhon', 'Smith', 1990, 'teacher', false];
+
+console.log('no operations :', jhon);
+
+// used to add an element to the array
+jhon.push('blue');
+console.log('pushed blue', jhon);
+
+// adds an element to the first of the array
+jhon.unshift('Mr.');
+console.log('after unshift', jhon);
+
+// removes an element from the start of the array
+jhon.shift();
+console.log('shift executed : ', jhon);
+
+// used to remove the last element of an array
+jhon.pop();
+console.log('after pop :', jhon);
+
+// indexOf returns the position of the arument to the method inside the array
+console.log('index of teacher : ', jhon.indexOf('teacher'));
