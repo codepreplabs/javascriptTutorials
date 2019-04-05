1be96f369c32cb1b99775a0a2beb4e45c9f98b61
praveen <thejavamonk@gmail.com>
2019-04-05 15:22:27 +0530

    example on closures

---
+++ 14closures/closures.js
@@ -0,0 +1,13 @@
+// closures
+
+function retirement(retirementAge) {
+    
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(65);
+retirementUS(1990);
