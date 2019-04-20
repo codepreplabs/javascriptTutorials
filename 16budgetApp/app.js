6c5db8987f454f00bd19cbeda98cb2ae5334b341
praveen <thejavamonk@gmail.com>
2019-04-20 21:28:30 +0530

    updated project with code to manipulate HTML

--- 16budgetApp/app.js
+++ 16budgetApp/app.js
@@ -1,6 +1,46 @@
 var budgetController = (function(){
     
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
     
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allitems: {
+            exp: [],
+            inc: []
+        },
+        totals:{
+            exp: 0,
+            inc: 0
+        }
+    }
+    
+    return {
+        addItem: function(type, desc, val){
+            var newItem, Id;
+            
+            if(data.allitems[type].length > 0){
+               Id = data.allitems[type][data.allitems[type].length - 1].id + 1;
+            } else {
+               Id = 0;
+            }
+            if('inc' === type){
+                newItem = new Income(Id, desc, val);
+            }else{
+                newItem = new Expense(Id, desc, val);
+            }
+            data.allitems[type].push(newItem);
+            return newItem;
+        }
+    }
 })();
 
 
@@ -10,7 +50,9 @@ var uiController = (function(){
         typeClass: '.add__type',
         descClass: '.add__description',
         valueClass: '.add__value',
-        btnClass: '.add__btn'
+        btnClass: '.add__btn',
+        incomeListClass: '.income__list',
+        expenseListClass: '.expense__list'
         }
     
     return {
@@ -21,6 +63,35 @@ var uiController = (function(){
                 expDesc: document.querySelector(uiDomStrings.descClass).value,
                 expAmt: document.querySelector(uiDomStrings.valueClass).value
             }
+        },
+        
+        addListItem: function(obj, type){
+            
+            var html, htmlWithData, element;
+            
+            // create HTML string with place holder text
+            if('inc' === type){
+                element = uiDomStrings.incomeListClass;
+               html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+            } else {
+                element = uiDomStrings.expenseListClass;
+               html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">- %value%</div> <div class="item__percentage">%itemPercent%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div> </div>';
+            }
+            
+            // replace the placeholder text with some actual data
+            htmlWithData = html.replace('%id%', obj.id);
+            htmlWithData = htmlWithData.replace('%value%', obj.value);
+            htmlWithData = htmlWithData.replace('%description%', obj.description);
+            
+/*          
+            if('exp' === type){
+                htmlWithData = html.replace('%itemPercent%', );
+            }
+*/
+           
+            
+            // insert the html in to the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', htmlWithData);
         },
         
         getDomStrings: function(){
@@ -48,9 +119,12 @@ var controller = (function(budgetCtlr, uiCtlr){
     var ctrlAddItem = function(){
         
        // 1. get the field input data
-        console.log(uiCtlr.getInputData());
+        var input = uiCtlr.getInputData();
        // 2. add item to the budget controller
+        var newItem = budgetCtlr.addItem(input.expenseType, input.expDesc, input.expAmt);
+        console.log(newItem);
        // 3. add item to UI
+        uiCtlr.addListItem(newItem, input.expenseType);
        // 4. calculate the budget
        // 5. display the budget to UI
         console.log('invoked ctrlAddItem')
