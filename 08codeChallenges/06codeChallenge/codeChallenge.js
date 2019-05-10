dc0cd38e9db44afa45775474f125058eeed4f34a
praveen <thejavamonk@gmail.com>
2019-05-10 16:11:13 +0530

    ES6 code challenge

---
+++ 08codeChallenges/06codeChallenge/codeChallenge.js
@@ -0,0 +1,26 @@
+/*
+
+We are administering a small town and are incharge of two elements
+
+1. Parks
+2. Streets
+
+There are 3 parks and 4 streets
+
+all of them have a name and a build year
+
+At the end of the year we want a final report with the following:
+
+1. Tree density of each park in the town (no. of trees / park area)
+2. Average age of each towns park (sum of all ages / number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the towns streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown the default is normal.
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrowfunctions,destructuring
+*/
+
+
+
