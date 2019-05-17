f22a3bbbc7f0977e252e639691da1cfd7bd1ae57
praveen <thejavamonk@gmail.com>
2019-05-17 13:12:21 +0530

    completed configuration of babel

--- 19forkify/src/js/index.js
+++ 19forkify/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import x from './test'
-
-console.log(`Imported ${x} from some other module`);
+const y = 23;
+console.log(`Imported ${x} from some other module. value y is ${y}`);
